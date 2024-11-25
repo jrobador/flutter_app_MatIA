f01604dd78d6929d8e3f04558485509f13d5d31b
Joaquín Robador <joaquinrobador@gmail.com>
2024-11-25 06:07:26 -0300

    updated

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -27,16 +27,17 @@ const RESOURCES = {".firebase/hosting.bG9ncw.cache": "97c00bc183076ac285f6f12de9
 "firebase-debug.log": "3c2e848624f2ca11ef2f3a988d423ca5",
 "firebase.json": "2b6734cb09d4cc85aa6694b35f194213",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "c60e44fb672b888081b644a80b1b07dd",
+"flutter_bootstrap.js": "34a239e1be9b3cb5d3044b27fb0d682c",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "133912e3b208ea748aaa2a6c42711b3b",
-"/": "133912e3b208ea748aaa2a6c42711b3b",
+"index.html": "3e5ef080ae89f312e66e770c7b73630b",
+"/": "3e5ef080ae89f312e66e770c7b73630b",
 "logs/index.html": "cda9713b80652fca783a1d25b069299b",
-"main.dart.js": "39ecb34408d10464f13d06f5fd7cbead",
+"main.dart.js": "56898b57044e8b25f2c67097d81525c7",
 "manifest.json": "34d7a93a24a9edfdcbc76b9763f73a11",
+"matIA_logo.png": "2858a24700756007e3be366349c13fa3",
 "version.json": "0a78011d6d8e8dd503b973b8da95d0f0"};
 // The application shell files that are downloaded before a service worker can
 // start.
